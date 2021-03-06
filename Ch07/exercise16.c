05c5d6cfe2dc7627b1dec5c4faa88b68add7938d
Ismael <ee.ismaelg@gmail.com>
2021-03-06 13:43:36 -0800

    Add files via upload

---
+++ Ch07/exercise16.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int         convertedNumber[64];
+long int    numberToConvert;
+int         base;
+int         digit = 0;
+
+void getNumberAndBase (void)
+{
+    bool validBase = false;
+
+    printf("Number to be converted? ");
+    scanf("%li", &numberToConvert);
+
+    if ( numberToConvert == 0)
+        exit(0);
+
+    while (!validBase){
+        printf("Base? ");
+        scanf("%i", &base);
+        if ( base < 2 || base > 16 ){
+            printf("Bad base - must be between 2 and 16\n");
+        }
+        else
+            validBase = true;
+    }
+}
+
+void convertNumber (void)
+{
+    do {
+        convertedNumber[digit] = numberToConvert % base;
+        ++digit;
+        numberToConvert /= base;
+    }
+    while (numberToConvert != 0);
+}
+
+void displayConvertedNumber (void)
+{
+    const char baseDigits[16] =
+            { '0', '1', '2', '3', '4', '5', '6', '7',
+              '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    int nextDigit;
+
+    printf("Converted number = ");
+
+    for ( --digit; digit >= 0; --digit ){
+        nextDigit = convertedNumber[digit];
+        printf ("%c", baseDigits[nextDigit]);
+    }
+
+    printf("\n");
+}
+
+int main() {
+
+    getNumberAndBase();
+    convertNumber();
+    displayConvertedNumber();
+
+    return 0;
+}
