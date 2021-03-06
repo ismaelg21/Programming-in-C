2742b6f43e3a37ccf9c6637c8b5a922dd7f0c8f0
Ismael <ee.ismaelg@gmail.com>
2021-03-06 14:00:01 -0800

    Update exercise2.c

--- Ch07/exercise2.c
+++ Ch07/exercise2.c
@@ -1,58 +1,27 @@
+#include <stdio.h>
 
-****************************************
-What Lore Received
+int calculateTriangularNumber (int n)
+{
+    int i, triangularNumber = 0;
 
-Owed to Lore		240.00
-Zelle 	10/29 		1923.00
-Zelle 	11/09		2928.00
-Zelle 	11/10		216.00
-Zelle 	11/23		1800.00
-Zelle 	12/09		1800.00
-Zelle 	12/16		267.50
-Zelle 	12/28		2025.00
+    for (i = 1; i <= n; i++)
+        triangularNumber += i;
 
-Total 				11199.50
+    return triangularNumber;
+}
 
-*****************************************
+int main() {
 
-What Lore Spent
+    int number, triangularNumber, counter;
 
-Pizza Hut 			103.49
-Chic Fil-A   		27.78
-Chic Fil-A			10.06
-Chic Fil-A			10.06
-Nintendo(Jr) 		50.00
-Amazon(Zelle)		126.79
-Money to Tachi		200.00
-Deco Styles			540.00
-Chic Fil-A 11/27	21.33
-Printer(Best Buy)	253.98
-Nintendo 11/27		20.00
-Turkey				100.00
-Chic Fil-A			54.09
-Adidas(12/04) 		221.00
-Withdraw			4000.00
-Chic Fil-A 12/8 	21.33
-Abercrombie			336.88
-Abercrombie			127.00			*after return*
-Chic Fil-A 12/12	21.33
-Nintendo 12/14		20.00
-Withdraw			2000.00
-Best Buy (watch)	533.44
-Nintendo 12/17		35.00
-Amazon (Switch)		19.70
-Nintendo 12/30		25.00
+    for (counter = 1; counter <= 5; counter++){
+        printf("What triangular number do you want? ");
+        scanf("%i", &number);
 
+        triangularNumber = calculateTriangularNumber(number);
 
-Total 				8878.26
+        printf("Triangular number %i is %i\n\n", number, triangularNumber);
+    }
 
-
-*****************************************
-
-Mom Owe me
-
-OMEGA XL			82.05 
-Wagon				109.45
-Abercrombie			50.00
-OMEGA XL			82.05
-
+    return 0;
+}
